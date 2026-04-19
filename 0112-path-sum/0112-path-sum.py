c8bc31905e6496097ad24e2746cfcaabd24bc3b7
Minhal Haider Naqvi <minhalhaiderofficial2003@gmail.com>
2026-04-19 17:39:31 +0530

    Time: 3 ms (29.75%), Space: 20.3 MB (29.59%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -3,10 +3,10 @@ class Solution:
         if not root:
             return False
         
-        # If it's a leaf node
+        # agar leaf node hai
         if not root.left and not root.right:
             return targetSum == root.val
         
-        # Check left or right subtree
+        # left ya right me check karo
         return (self.hasPathSum(root.left, targetSum - root.val) or
                 self.hasPathSum(root.right, targetSum - root.val))
